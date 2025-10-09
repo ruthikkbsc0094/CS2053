90318b99fe0e76bf8223fb81a7482169ba1c9691
ruthikkbsc0094 <ruthikkbsc24@rvu.edu.in>
2025-10-09 12:02:07 +0530

    Lab_Session_1.java

--- BookDemo.java
+++ BookDemo.java
@@ -1,16 +1,15 @@
+// Lab Session:1
 class Book {
     String title;
     String author;
     int year;
 
-    // Constructor
     Book(String title, String author, int year) {
-        this.title = title;     // Using 'this' to refer to the current object's fields
+        this.title = title;     
         this.author = author;
         this.year = year;
     }
 
-    // Method to display book details
     void displayDetails() {
         System.out.println("Title : " + this.title);
         System.out.println("Author: " + this.author);
@@ -20,11 +19,9 @@ class Book {
 
 public class BookDemo {
     public static void main(String[] args) {
-        // Create Book objects
         Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
         Book book2 = new Book("1984", "George Orwell", 1949);
 
-        // Display their details
         System.out.println("Book 1 Details:");
         book1.displayDetails();
 
@@ -32,3 +29,4 @@ public class BookDemo {
         book2.displayDetails();
     }
 }
+
